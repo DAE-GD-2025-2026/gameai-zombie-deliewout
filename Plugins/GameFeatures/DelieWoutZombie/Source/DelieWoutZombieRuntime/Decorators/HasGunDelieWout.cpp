93a89d71c3ed5b15b6dc8621c5b0c59eefafae39
deliewout <deliewout@gmail.com>
2026-06-06 20:22:43 +0200

    Added hasgun decorator

---
+++ Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/Decorators/HasGunDelieWout.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HasGunDelieWout.h"
+#include "AIController.h"
+#include "Common/InventoryComponent.h"
+#include "Items/Weapon.h"
+
+bool UHasGunDelieWout::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AAIController* Controller = OwnerComp.GetAIOwner();
+	if (!Controller) return false;
+
+	APawn* Pawn = Controller->GetPawn();
+	if (!Pawn)return false;
+
+	UInventoryComponent* Inventory = Pawn->FindComponentByClass<UInventoryComponent>();
+	if (!Inventory)return false;
+
+	for (const auto& Item : Inventory->GetInventory())
+	{
+		if (Item == nullptr) continue;
+		if (Item->GetItemType() == EItemType::Pistol || Item->GetItemType() == EItemType::Shotgun)
+			return true;
+	}
+
+	return false;
+}
---
+++ Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/Decorators/HasGunDelieWout.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "HasGunDelieWout.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DELIEWOUTZOMBIERUNTIME_API UHasGunDelieWout : public UBTDecorator
+{
+	GENERATED_BODY()
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
