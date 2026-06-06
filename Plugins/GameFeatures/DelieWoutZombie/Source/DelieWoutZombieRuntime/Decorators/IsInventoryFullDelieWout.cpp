09353cdc50c12fec98e48d146591c4cc1869682c
deliewout <deliewout@gmail.com>
2026-06-06 12:19:40 +0200

    Finished the IsInventoryFull decorator

--- Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/Decorators/IsInventoryFullDelieWout.cpp
+++ Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/Decorators/IsInventoryFullDelieWout.cpp
@@ -2,8 +2,27 @@
 
 
 #include "IsInventoryFullDelieWout.h"
+#include "AIController.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Common/InventoryComponent.h"
+#include "Items/ItemManager.h"
 
 bool UIsInventoryFullDelieWout::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
 {
-	return false;
+	AAIController* Controller = OwnerComp.GetAIOwner();
+	if (!Controller) return false;
+
+	APawn* Pawn = Controller->GetPawn();
+	if (!Pawn) return false;
+
+	UInventoryComponent* Inventory = Pawn->GetComponentByClass<UInventoryComponent>();
+	if (!Inventory) return false;
+
+	auto Items = Inventory->GetInventory();
+	for (int32 i = 0; i < Items.Num(); ++i)
+	{
+		if (Items[i] == nullptr)
+			return false;
+	}
+	return true;
 }
