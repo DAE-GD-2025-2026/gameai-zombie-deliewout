9d8588ec5d21089b71ac6645e368803683f7b498
deliewout <deliewout@gmail.com>
2026-06-07 12:31:31 +0200

    Added DidEnemyBite decorator

---
+++ Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/Decorators/DidEnemyBiteDelieWout.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DidEnemyBiteDelieWout.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+
+bool UDidEnemyBiteDelieWout::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AAIController* Controller = OwnerComp.GetAIOwner();
+	if (!Controller)return false;
+
+	UBlackboardComponent* BB = Controller->GetBlackboardComponent();
+	if (!BB)return false;
+
+	return BB->GetValueAsBool("WasBitten");
+}
--- Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/StudentPerceptorDelieWout.cpp
+++ Plugins/GameFeatures/DelieWoutZombie/Source/DelieWoutZombieRuntime/StudentPerceptorDelieWout.cpp
@@ -2,6 +2,8 @@
 
 
 #include "StudentPerceptorDelieWout.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 UStudentPerceptorDelieWout::UStudentPerceptorDelieWout()
@@ -22,5 +24,21 @@ void UStudentPerceptorDelieWout::BeginPlay()
 void UStudentPerceptorDelieWout::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
 	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green,
-	FString::Printf(TEXT("Saw Something!")));
+		FString::Printf(TEXT("Saw Something!")));
+
+	APawn* Pawn = Cast<APawn>(GetOwner());
+	if (!Pawn) return;
+
+	AAIController* AIController = Cast<AAIController>(Pawn->GetController());
+	if (!AIController) return;
+
+	UBlackboardComponent* BB = AIController->GetBlackboardComponent();
+	if (!BB) return;
+
+	UAIPerceptionComponent* PerceptionComp = Pawn->FindComponentByClass<UAIPerceptionComponent>();
+	if (!PerceptionComp) return;
+
+	TArray<AActor*> ZombieBiters;
+	PerceptionComp->GetCurrentlyPerceivedActors(UAISense_Damage::StaticClass(), ZombieBiters);
+	BB->SetValueAsBool("WasBitten", ZombieBiters.Num() > 0);
 }
